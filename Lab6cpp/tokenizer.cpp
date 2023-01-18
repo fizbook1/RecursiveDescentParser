93c6b72539fbe30831ce75a819d16e06aad63ea8
fizbook1 <77051952+fizbook1@users.noreply.github.com>
2023-01-18 09:31:57 +0100

    skicka upp

--- Lab6cpp/tokenizer.cpp
+++ Lab6cpp/tokenizer.cpp
@@ -1,10 +1,21 @@
 #include "tokenizer.h"
+#include <iterator>
+#include <sstream>
 std::vector<std::string> tokenizer::tokenize(std::string _line) {
 
 	line = _line;
 	int position = 0;
 	bool done = false;
 	std::vector<std::string> tokens;
+
+	char* skoob = _line.cstr();
+	char* p = strtok(skoob, " ");
+	while (p) {
+		printf("Token: %s\n", p);
+		tokens.push_back(std::string(p));
+		p = strtok(NULL, " ");
+	}
+
 
 	while (!done)
 	{
